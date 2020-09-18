ea5486ee153978c423939674796b811edf09a90b
Oleg Shchegolev <madindahouse@gmail.com>
2020-09-18 23:27:26 +0300

    add script for shredder object

--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Shredder : MonoBehaviour
 {
+    // проверяем столкновения с объектом Shredder
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(collision.gameObject);
+        // collision - передается, когда другой обьект входит в триггер присоединенный к обьекту Shredder
+        Destroy(collision.gameObject); // уничтожаем игровые объекты, которые столкнулись с Shredder
     }
 }
